c9ea1c6f9fde24ed9124079d190c472b05c34919
ermiyasassegu <67770426+ermiyasassegu@users.noreply.github.com>
2021-01-18 12:51:32 +0200

    Add files via upload

---
+++ Viope/ex1.1/ex1.2.c
@@ -0,0 +1,40 @@
+/*
+Printing on the screen
+Write a functioning program in C that, upon execution, prints the following on
+the screen: "Hello everybody!", followed by a newline character \n
+after the exclamation point. After printing, the program closes itself.
+*/
+
+#include<stdio.h>
+
+int main(){
+
+  printf("Hello everybody!");
+
+  return 0;
+}
+
+
+
+/*
+Reading from the keyboard and printing
+Write a program that prompts the user for an integer, calculates the square and
+prints the result on the screen (also print out a newline character \n after the
+result). After printing, the program closes itself.
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    int num1,sqr=0;
+
+    printf("\nEnter an integer:");
+    scanf("%d",&num1);
+
+    sqr = num1 * num1;
+
+    printf("\nThe square of the number you entered is %d",sqr);
+
+    return 0;
+}
